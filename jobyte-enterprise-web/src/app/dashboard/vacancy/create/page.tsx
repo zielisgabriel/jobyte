bf1c71aff194411e4d16b3a9ad3974a6e899f1a5
zielisgabriel <josegabrielas0586@email.com>
2025-11-22 18:20:53 -0300

    refactor: replace BackButton with Button component for navigation in CreateVacancyPage

--- jobyte-enterprise-web/src/app/dashboard/vacancy/create/page.tsx
+++ jobyte-enterprise-web/src/app/dashboard/vacancy/create/page.tsx
@@ -1,14 +1,23 @@
+"use client";
+
 import { CreateVacancyForm } from "@/components/CreateVacancyForm";
-import { Separator } from "@/components/ui/separator";
-import { BackButton } from "@/components/BackButton";
+import { Button } from "@/components/ui/Button";
+import { ArrowLeftIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { Separator } from "radix-ui";
 
 export default function CreateVacancyPage() {
+  const router = useRouter();
+
   return (
     <main>
       <div className="mt-12 w-xl mx-auto">
-        <BackButton />
+        <Button className="mb-5" onClick={() => router.back()}>
+          <ArrowLeftIcon size={16} />
+          Voltar
+        </Button>
         <h1 className="font-bold text-3xl">Criar nova vaga</h1>
-        <Separator className="my-4" />
+        <Separator.Root className="my-4 bg-foreground w-full h-px" />
         <CreateVacancyForm />
       </div>
     </main>
