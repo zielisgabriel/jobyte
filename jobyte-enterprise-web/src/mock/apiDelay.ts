87351515f94fbaebaff447ef19001a8b98ae876f
zielisgabriel <josegabrielas0586@email.com>
2025-11-22 18:19:41 -0300

    refactor: remove unused components and services related to authentication

--- jobyte-enterprise-web/src/mock/apiDelay.ts
+++
@@ -1,3 +0,0 @@
-export async function apiDelay(ms: number) {
-  return new Promise(resolver => setTimeout(resolver, ms));
-}
--- jobyte-enterprise-web/src/services/refreshTokenService.ts
+++
@@ -1,8 +0,0 @@
-export async function refreshTokenService(refreshToken: string): Promise<Response> {
-  return await fetch(`${process.env.PUBLIC_API_URL}/api/auth/enterprise/refresh-token`, {
-    method: "POST",
-    headers: {
-      "Cookie": `refresh_token=${refreshToken}`
-    }
-  });
-}
