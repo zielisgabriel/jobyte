ceaedd85aef39b7df80e17fe4eedbef15a8f8d7e
zielisgabriel <josegabrielas0586@email.com>
2025-11-29 18:39:00 -0300

    refactor(register): clean up code formatting and improve readability

--- jobyte-enterprise-web/src/app/register/page.tsx
+++ jobyte-enterprise-web/src/app/register/page.tsx
@@ -5,30 +5,36 @@ import { Button } from "@/components/ui/button";
 import { useMobile } from "@/hooks/useMobile";
 import clsx from "clsx";
 import { ArrowLeft } from "lucide-react";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 
 export default function RegisterPage() {
-  const {isMobile} = useMobile();
+  const { isMobile } = useMobile();
   const router = useRouter();
 
   return (
-    <main className={twMerge(clsx("grid grid-cols-[1fr_1fr] h-screen", isMobile && "flex"))}>
-      <div className={twMerge(clsx("flex flex-col justify-center items-center bg-background", isMobile && "hidden"))}>
+    <main
+      className={twMerge(
+        clsx("grid grid-cols-[1fr_1fr] h-screen", isMobile && "flex")
+      )}
+    >
+      <div
+        className={twMerge(
+          clsx(
+            "flex flex-col justify-center items-center bg-background",
+            isMobile && "hidden"
+          )
+        )}
+      >
         <span className="flex items-center">
-          <h1 className="font-black text-5xl">
-            Jobyte.
-          </h1>
-          <p className="text-xl font-semibold">
-            nterprise
-          </p>
+          <h1 className="font-black text-5xl">Jobyte.</h1>
+          <p className="text-xl font-semibold">nterprise</p>
         </span>
       </div>
       <div className="relative w-full p-2 bg-foreground">
         <Button
-          className="absolute left-1 top-1"
-          variant={"color_invert"}
+          className="absolute left-1 top-1 text-background hover:text-foreground"
+          variant="ghost"
           onClick={() => router.back()}
         >
           <ArrowLeft />
