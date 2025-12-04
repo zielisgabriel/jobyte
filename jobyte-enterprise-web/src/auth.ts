521a15d876b2c0b0e811191b8243c914ee985db9
zielisgabriel <josegabrielas0586@email.com>
2025-12-04 19:57:12 -0300

    feat(auth): implement NextAuth with Keycloak provider for authentication

---
+++ jobyte-enterprise-web/src/auth.ts
@@ -0,0 +1,29 @@
+import NextAuth from "next-auth";
+import Keycloak from "next-auth/providers/keycloak";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  debug: true,
+  trustHost: true,
+  providers: [
+    Keycloak({
+      checks: ["nonce"],
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.refreshToken = account.refresh_token;
+        token.expiresAt = account.expires_at;
+        token.idToken = account.id_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken as string;
+      session.refreshToken = token.refreshToken as string;
+      session.expiresAt = token.expiresAt as number;
+      return session;
+    },
+  },
+});
