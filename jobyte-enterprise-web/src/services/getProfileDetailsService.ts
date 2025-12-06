b7576ee9e3d5769536e1a99ec406ac16015ed2dc
zielisgabriel <josegabrielas0586@email.com>
2025-12-06 00:34:58 -0300

    fix(getProfileDetailsService): update API endpoint to fetch profile details

--- jobyte-enterprise-web/src/services/getProfileDetailsService.ts
+++ jobyte-enterprise-web/src/services/getProfileDetailsService.ts
@@ -1,5 +1,5 @@
 import { fetchWithAuth } from "@/middlewares/fetchWithAuth";
 
 export async function getProfileDetailsService() {
-  return await fetchWithAuth("/api/enterprise/profile/me");
+  return await fetchWithAuth("/api/enterprise/profile/details");
 }
