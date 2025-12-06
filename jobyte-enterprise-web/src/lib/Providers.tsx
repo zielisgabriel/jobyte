50bc610ed6d4259fd4b7b8f82d9967acf0171fc7
zielisgabriel <josegabrielas0586@email.com>
2025-12-06 00:34:42 -0300

    feat(providers): add Providers component for session and query management

---
+++ jobyte-enterprise-web/src/lib/Providers.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "@/components/ThemeProvider";
+import { SessionProvider } from "next-auth/react";
+import { useState } from "react";
+
+interface ProvidersProps {
+  children: React.ReactNode;
+}
+
+export function Providers({ children }: ProvidersProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <SessionProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </QueryClientProvider>
+    </SessionProvider>
+  );
+}
