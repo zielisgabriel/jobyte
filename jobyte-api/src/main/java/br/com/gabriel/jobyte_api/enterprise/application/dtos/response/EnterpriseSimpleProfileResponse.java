89d400c11c18c347d148684320321f0b72507c6f
zielisgabriel <josegabrielas0586@email.com>
2025-12-07 12:49:49 -0300

    fix(EnterpriseSimpleProfileResponse): add UUID field to response DTO

--- jobyte-api/src/main/java/br/com/gabriel/jobyte_api/enterprise/application/dtos/response/EnterpriseSimpleProfileResponse.java
+++ jobyte-api/src/main/java/br/com/gabriel/jobyte_api/enterprise/application/dtos/response/EnterpriseSimpleProfileResponse.java
@@ -1,12 +1,16 @@
 package br.com.gabriel.jobyte_api.enterprise.application.dtos.response;
 
+import java.util.UUID;
+
 import br.com.gabriel.jobyte_api.enterprise.domain.entities.EnterpriseProfile;
 
 public record EnterpriseSimpleProfileResponse(
+  UUID id,
   String companyName
 ) {
   public static EnterpriseSimpleProfileResponse fromDomain(EnterpriseProfile entity) {
     return new EnterpriseSimpleProfileResponse(
+      entity.getId(),
       entity.getCompanyName()
     );
   }
