fa9b6b6be999b296835388f162bb8f05fc1240b0
zielisgabriel <josegabrielas0586@email.com>
2025-12-07 12:51:24 -0300

    fix(next.config.ts): ensure proper configuration syntax by adding a comma

--- jobyte-enterprise-web/next.config.ts
+++ jobyte-enterprise-web/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: false
+  devIndicators: false,
+  cacheComponents: true
 };
 
 export default nextConfig;
