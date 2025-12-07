0bd8c2669f45edfd7dfa1c0d9d5c60278c6a41dc
zielisgabriel <josegabrielas0586@email.com>
2025-12-07 12:52:19 -0300

    fix(route.ts): refactor GET method to use fetchWithoutAuth for improved request handling

--- jobyte-enterprise-web/src/app/api/metrics/selection-processes/[id]/route.ts
+++ jobyte-enterprise-web/src/app/api/metrics/selection-processes/[id]/route.ts
@@ -1,7 +1,18 @@
+import { fetchWithoutAuth } from "@/middlewares/fetchWithoutAuth";
 import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
-  return await fetch(`http://localhost:5000/api/metrics/selection-processes/${(await params).id}`, {
-    method: "GET",
-  });
+  const {
+    id: enterpriseId
+  } = await params;
+
+  console.log("route.ts: enterpriseId:", enterpriseId);
+
+  return await fetchWithoutAuth({
+    path: `/api/metrics/selection-processes/${enterpriseId}`,
+    host: "http://localhost:5000",
+    init: {
+      method: req.method
+    }
+  })
 }
