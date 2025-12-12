b31539fb58cbd748044ed0c8aa3b9159ccdda3b5
zielisgabriel <josegabrielas0586@email.com>
2025-12-11 22:58:05 -0300

    refactor(getProfileDetailsService): enhance caching and response handling

--- jobyte-enterprise-web/src/services/getProfileDetailsService.ts
+++ jobyte-enterprise-web/src/services/getProfileDetailsService.ts
@@ -1,5 +1,18 @@
 import { fetchWithAuth } from "@/middlewares/fetchWithAuth";
+import { ProfileDetails } from "@/types/ProfileDetails";
+import { cacheLife, cacheTag } from "next/cache";
 
-export async function getProfileDetailsService() {
-  return await fetchWithAuth("/api/enterprise/profile/details");
+export async function getProfileDetailsService(): Promise<ProfileDetails> {
+  "use cache: private";
+
+  cacheTag("profileDetails");
+  cacheLife("minutes");
+
+  const response = await fetchWithAuth({
+    path: "/api/enterprise/profile/details"
+  });
+
+  const data: ProfileDetails = await response.json();
+
+  return data;
 }
