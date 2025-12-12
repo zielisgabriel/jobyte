a84c59cd8ac6aab73d15b427b59bdba150d1844f
zielisgabriel <josegabrielas0586@email.com>
2025-12-11 22:58:29 -0300

    feat(ProfileDetails): create ProfileDetails interface for user profile data

---
+++ jobyte-enterprise-web/src/types/ProfileDetails.d.ts
@@ -0,0 +1,10 @@
+export interface ProfileDetails {
+  id: string;
+  companyName: string;
+  cnpj: string;
+  address: string;
+  phone: string;
+  createdAt: string;
+  updatedAt: string;
+  keycloakUserId: string;
+}
