7206de0d16918637bd808ab622cc72e7c3cb79fb
zielisgabriel <josegabrielas0586@email.com>
2025-12-14 20:26:32 -0300

    feat(getVacancyDetailsByIdService): refactor to use fetchClient for API calls

--- jobyte-enterprise-web/src/services/get-vacancy-details-by-id-service.ts
+++ jobyte-enterprise-web/src/services/get-vacancy-details-by-id-service.ts
@@ -1,10 +1,14 @@
-import { fetchWithAuth } from "@/middlewares/fetch-with-auth";
+import { fetchClient } from "@/lib/fetch-client";
+import { Vacancy } from "@/types/Vacancy";
 
-export async function getVacancyDetailsByIdService(id: string) {
-  return await fetchWithAuth({
-      path: `/api/enterprise/vacancy/${id}`,
-      init: {
-        method: "GET"
-      }
-    });
+export async function getVacancyDetailsByIdService(id: string): Promise<Vacancy> {
+  const response = await fetchClient({
+    path: `/api/enterprise/vacancy/${id}`,
+    init: {
+      method: "GET"
+    },
+    isAuth: true
+  });
+
+  return await response.json();
 }
