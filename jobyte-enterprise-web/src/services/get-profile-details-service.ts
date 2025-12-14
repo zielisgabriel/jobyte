3b7d7e865d881b9989151112bee7661b6c2d3115
zielisgabriel <josegabrielas0586@email.com>
2025-12-14 20:26:23 -0300

    feat(getProfileDetailsService): update to use fetchClient for API calls and allow null return type

--- jobyte-enterprise-web/src/services/get-profile-details-service.ts
+++ jobyte-enterprise-web/src/services/get-profile-details-service.ts
@@ -1,15 +1,21 @@
-import { fetchWithAuth } from "@/middlewares/fetch-with-auth";
+"use server";
+
+import { fetchClient } from "@/lib/fetch-client";
 import { ProfileDetails } from "@/types/ProfileDetails";
 import { cacheLife, cacheTag } from "next/cache";
 
-export async function getProfileDetailsService(): Promise<ProfileDetails> {
+export async function getProfileDetailsService(): Promise<ProfileDetails | null> {
   "use cache: private";
 
   cacheTag("profileDetails");
   cacheLife("minutes");
 
-  const response = await fetchWithAuth({
-    path: "/api/enterprise/profile/details"
+  const response = await fetchClient({
+    path: "/api/enterprise/profile/details",
+    isAuth: true,
+    init: {
+      method: "GET"
+    }
   });
 
   const data: ProfileDetails = await response.json();
