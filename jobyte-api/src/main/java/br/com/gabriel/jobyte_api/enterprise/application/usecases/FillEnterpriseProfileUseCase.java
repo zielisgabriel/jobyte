7ab527064178953cb865ea73521d71bec2100460
zielisgabriel <josegabrielas0586@email.com>
2025-12-25 21:22:11 -0300

    fix(usecase): add IdGenerator to FillEnterpriseProfileUseCase for profile ID generation

--- jobyte-api/src/main/java/br/com/gabriel/jobyte_api/enterprise/application/usecases/FillEnterpriseProfileUseCase.java
+++ jobyte-api/src/main/java/br/com/gabriel/jobyte_api/enterprise/application/usecases/FillEnterpriseProfileUseCase.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import br.com.gabriel.jobyte_api.enterprise.application.dtos.request.FillEnterpriseProfileRequest;
 import br.com.gabriel.jobyte_api.enterprise.domain.entities.EnterpriseProfile;
 import br.com.gabriel.jobyte_api.enterprise.domain.ports.EnterpriseRepositoryPort;
+import br.com.gabriel.jobyte_api.shared.domain.provider.IdGenerator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,11 +14,13 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class FillEnterpriseProfileUseCase {
   private final EnterpriseRepositoryPort repository;
+  private final IdGenerator idGenerator;
 
   public void execute(String keycloakUserId, FillEnterpriseProfileRequest request) {
-    log.info("Request: {}", request);
+    long profileId = this.idGenerator.nextId();
 
     EnterpriseProfile profile = EnterpriseProfile.create(
+      profileId,
       keycloakUserId,
       request.companyName(),
       request.cnpj(),
