0daca52e5b06dcaa49ae3b9890a3de8176454f8c
zielisgabriel <josegabrielas0586@email.com>
2025-12-26 19:28:50 -0300

    refactor(EnterpriseRepositoryPort): add methods to retrieve profile details and simple profile by Keycloak user ID

--- jobyte-api/src/main/java/br/com/gabriel/jobyte_api/enterprise/domain/ports/EnterpriseRepositoryPort.java
+++ jobyte-api/src/main/java/br/com/gabriel/jobyte_api/enterprise/domain/ports/EnterpriseRepositoryPort.java
@@ -3,12 +3,13 @@ package br.com.gabriel.jobyte_api.enterprise.domain.ports;
 import java.util.Optional;
 
 import br.com.gabriel.jobyte_api.enterprise.domain.entities.EnterpriseProfile;
+import br.com.gabriel.jobyte_api.enterprise.domain.valueobjects.EnterpriseDetailsValueObject;
+import br.com.gabriel.jobyte_api.enterprise.domain.valueobjects.EnterpriseSimpleValueObject;
 
 public interface EnterpriseRepositoryPort {
   Optional<EnterpriseProfile> findById(Long id);
   Optional<EnterpriseProfile> findByKeycloakUserId(String keycloakUserId);
+  Optional<EnterpriseDetailsValueObject> findProfileDetailsByKeycloakUserId(String keycloakUserId);
+  Optional<EnterpriseSimpleValueObject> findProfileSimpleByKeycloakUserId(String keycloakUserId);
   EnterpriseProfile save(EnterpriseProfile enterprise);
-  void delete(EnterpriseProfile enterprise);
-  boolean existsByKeycloakUserId(String keycloakUserId);
-  boolean existsByCnpj(String cnpj);
 }
