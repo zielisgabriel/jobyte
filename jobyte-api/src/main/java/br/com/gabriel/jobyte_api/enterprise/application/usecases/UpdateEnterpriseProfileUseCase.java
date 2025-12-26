0a7ba949c32d83460b633a79f3a8fc28ff9c8a36
zielisgabriel <josegabrielas0586@email.com>
2025-12-26 19:28:43 -0300

    refactor(UpdateEnterpriseProfileUseCase): change return type to void and update profile retrieval logic

--- jobyte-api/src/main/java/br/com/gabriel/jobyte_api/enterprise/application/usecases/UpdateEnterpriseProfileUseCase.java
+++ jobyte-api/src/main/java/br/com/gabriel/jobyte_api/enterprise/application/usecases/UpdateEnterpriseProfileUseCase.java
@@ -3,9 +3,9 @@ package br.com.gabriel.jobyte_api.enterprise.application.usecases;
 import org.springframework.stereotype.Service;
 
 import br.com.gabriel.jobyte_api.enterprise.application.dtos.request.UpdateEnterpriseProfileRequest;
-import br.com.gabriel.jobyte_api.enterprise.application.dtos.response.EnterpriseDetailsProfileResponse;
 import br.com.gabriel.jobyte_api.enterprise.domain.entities.EnterpriseProfile;
 import br.com.gabriel.jobyte_api.enterprise.domain.ports.EnterpriseRepositoryPort;
+import br.com.gabriel.jobyte_api.enterprise.domain.valueobjects.EnterpriseDetailsValueObject;
 import br.com.gabriel.jobyte_api.shared.domain.exception.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 
@@ -14,18 +14,20 @@ import lombok.RequiredArgsConstructor;
 public class UpdateEnterpriseProfileUseCase {
   private final EnterpriseRepositoryPort enterpriseRepository;
 
-  public EnterpriseDetailsProfileResponse execute(String keycloakUserId, UpdateEnterpriseProfileRequest request) {
-    EnterpriseProfile profile = enterpriseRepository
-      .findByKeycloakUserId(keycloakUserId)
-      .orElseThrow(() -> new EntityNotFoundException("Perfil da empresa", keycloakUserId));
+  public void execute(String keycloakUserId, UpdateEnterpriseProfileRequest request) {
+    EnterpriseDetailsValueObject profile = this.enterpriseRepository
+      .findProfileDetailsByKeycloakUserId(keycloakUserId)
+        .orElseThrow(() -> new EntityNotFoundException("Perfil da empresa"));
 
-    profile.updateProfile(
-      request.companyName(),
-      request.address(),
-      request.phone()
+    EnterpriseProfile profileUpdated = this.enterpriseRepository.findById(profile.id())
+      .orElseThrow(() -> new EntityNotFoundException("Perfil da empresa"));
+
+    profileUpdated.updateProfile(
+      profile.companyName(),
+      profile.address(),
+      profile.phone()
     );
 
-    EnterpriseProfile savedProfile = enterpriseRepository.save(profile);
-    return EnterpriseDetailsProfileResponse.fromDomain(savedProfile);
+    this.enterpriseRepository.save(profileUpdated);
   }
 }
