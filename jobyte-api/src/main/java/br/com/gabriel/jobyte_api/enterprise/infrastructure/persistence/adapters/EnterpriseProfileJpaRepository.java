785e1d888f00f6c80a56c878b9c141fe3ecf3b78
zielisgabriel <josegabrielas0586@email.com>
2025-12-25 21:22:56 -0300

    fix(entity): change id type from UUID to Long in EnterpriseProfileJpaRepository

--- jobyte-api/src/main/java/br/com/gabriel/jobyte_api/enterprise/infrastructure/persistence/adapters/EnterpriseProfileJpaRepository.java
+++ jobyte-api/src/main/java/br/com/gabriel/jobyte_api/enterprise/infrastructure/persistence/adapters/EnterpriseProfileJpaRepository.java
@@ -1,13 +1,12 @@
 package br.com.gabriel.jobyte_api.enterprise.infrastructure.persistence.adapters;
 
 import java.util.Optional;
-import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.gabriel.jobyte_api.enterprise.infrastructure.persistence.entities.EnterpriseProfileEntity;
 
-interface EnterpriseProfileJpaRepository extends JpaRepository<EnterpriseProfileEntity, UUID> {
+interface EnterpriseProfileJpaRepository extends JpaRepository<EnterpriseProfileEntity, Long> {
   Optional<EnterpriseProfileEntity> findByKeycloakUserId(String keycloakUserId);
   boolean existsByKeycloakUserId(String keycloakUserId);
   boolean existsByCnpj(String cnpj);
