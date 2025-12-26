3e51e904002904d5f3e172e8fa5aee7e3ad602f3
zielisgabriel <josegabrielas0586@email.com>
2025-12-25 21:22:48 -0300

    fix(entity): change id type from UUID to Long in EnterpriseRepositoryPort

--- jobyte-api/src/main/java/br/com/gabriel/jobyte_api/enterprise/domain/ports/EnterpriseRepositoryPort.java
+++ jobyte-api/src/main/java/br/com/gabriel/jobyte_api/enterprise/domain/ports/EnterpriseRepositoryPort.java
@@ -1,12 +1,11 @@
 package br.com.gabriel.jobyte_api.enterprise.domain.ports;
 
 import java.util.Optional;
-import java.util.UUID;
 
 import br.com.gabriel.jobyte_api.enterprise.domain.entities.EnterpriseProfile;
 
 public interface EnterpriseRepositoryPort {
-  Optional<EnterpriseProfile> findById(UUID id);
+  Optional<EnterpriseProfile> findById(Long id);
   Optional<EnterpriseProfile> findByKeycloakUserId(String keycloakUserId);
   EnterpriseProfile save(EnterpriseProfile enterprise);
   void delete(EnterpriseProfile enterprise);
