ef42399fb08a4cf1b10ba548df8edb502a269dbb
zielisgabriel <josegabrielas0586@email.com>
2025-12-25 21:24:21 -0300

    refactor(exception): remove BusinessException, EntityNotFoundException, and ValidationException classes

--- jobyte-api/src/main/java/br/com/gabriel/jobyte_api/shared/domain/exceptions/BusinessException.java
+++
@@ -1,9 +0,0 @@
-package br.com.gabriel.jobyte_api.shared.domain.exceptions;
-
-import jakarta.ws.rs.BadRequestException;
-
-public class BusinessException extends BadRequestException {
-  public BusinessException(String message) {
-    super(message);
-  }
-}
--- jobyte-api/src/main/java/br/com/gabriel/jobyte_api/shared/domain/exceptions/ValidationException.java
+++
@@ -1,9 +0,0 @@
-package br.com.gabriel.jobyte_api.shared.domain.exceptions;
-
-import jakarta.ws.rs.BadRequestException;
-
-public class ValidationException extends BadRequestException {
-  public ValidationException(String message) {
-    super(message);
-  }
-}
