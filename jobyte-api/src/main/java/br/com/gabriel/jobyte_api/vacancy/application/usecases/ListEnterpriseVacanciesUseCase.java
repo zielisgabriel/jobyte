5a66d2a3951c46b7270c055392a2bc7c3697530f
zielisgabriel <josegabrielas0586@email.com>
2025-12-25 21:26:14 -0300

    refactor(vacancy): change enterpriseId type from UUID to Long in ListEnterpriseVacanciesUseCase

--- jobyte-api/src/main/java/br/com/gabriel/jobyte_api/vacancy/application/usecases/ListEnterpriseVacanciesUseCase.java
+++ jobyte-api/src/main/java/br/com/gabriel/jobyte_api/vacancy/application/usecases/ListEnterpriseVacanciesUseCase.java
@@ -1,7 +1,6 @@
 package br.com.gabriel.jobyte_api.vacancy.application.usecases;
 
 import java.util.List;
-import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
@@ -17,7 +16,7 @@ public class ListEnterpriseVacanciesUseCase {
   private final VacancyRepository vacancyRepository;
   private static final int PAGE_SIZE = 20;
 
-  public VacancyListResponse execute(UUID enterpriseId, int page) {
+  public VacancyListResponse execute(Long enterpriseId, int page) {
     List<Vacancy> vacancies = vacancyRepository.findByEnterpriseId(enterpriseId, page - 1, PAGE_SIZE);
     long totalElements = vacancyRepository.countByEnterpriseId(enterpriseId);
     int totalPages = (int) Math.ceil((double) totalElements / PAGE_SIZE);
