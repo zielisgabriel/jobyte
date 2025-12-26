fb5faa79be647f026b5955dde300e0ea9a5c2862
zielisgabriel <josegabrielas0586@email.com>
2025-12-25 21:28:07 -0300

    refactor(vacancy): change id and enterpriseId types from UUID to Long in VacancyEntity

--- jobyte-api/src/main/java/br/com/gabriel/jobyte_api/vacancy/infrastructure/persistence/entities/VacancyEntity.java
+++ jobyte-api/src/main/java/br/com/gabriel/jobyte_api/vacancy/infrastructure/persistence/entities/VacancyEntity.java
@@ -1,15 +1,12 @@
 package br.com.gabriel.jobyte_api.vacancy.infrastructure.persistence.entities;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 import br.com.gabriel.jobyte_api.vacancy.domain.enums.VacancyStatus;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
@@ -25,9 +22,8 @@ import lombok.NoArgsConstructor;
 @Table(name = "vacancies")
 public class VacancyEntity {
   @Id
-  @GeneratedValue(strategy = GenerationType.UUID)
   @Column(name = "id", updatable = false, nullable = false)
-  private UUID id;
+  private Long id;
 
   @Column(name = "title", nullable = false)
   private String title;
@@ -36,7 +32,7 @@ public class VacancyEntity {
   private String description;
 
   @Column(name = "enterprise_id", nullable = false)
-  private UUID enterpriseId;
+  private Long enterpriseId;
 
   @Enumerated(EnumType.STRING)
   @Column(name = "status", nullable = false)
