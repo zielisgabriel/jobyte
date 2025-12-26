6c88c46ba7d0b6693f239476c034637bda39412e
zielisgabriel <josegabrielas0586@email.com>
2025-12-25 21:23:47 -0300

    feat(exception): create EntityNotFoundException class extending NotFoundException

---
+++ jobyte-api/src/main/java/br/com/gabriel/jobyte_api/shared/domain/exception/EntityNotFoundException.java
@@ -0,0 +1,13 @@
+package br.com.gabriel.jobyte_api.shared.domain.exception;
+
+import jakarta.ws.rs.NotFoundException;
+
+public class EntityNotFoundException extends NotFoundException {
+  public EntityNotFoundException(String message) {
+    super(message);
+  }
+
+  public EntityNotFoundException(String entity, Object id) {
+    super(String.format("%s não encontrado(a) com id: %s", entity, id));
+  }
+}
