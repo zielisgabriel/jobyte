f0de1640cf30a5aa0b3866b9bad1027fccefddd5
zielisgabriel <josegabrielas0586@email.com>
2025-12-25 21:21:33 -0300

    fix(response): change id type from UUID to Long in EnterpriseDetailsProfileResponse

--- jobyte-api/src/main/java/br/com/gabriel/jobyte_api/enterprise/application/dtos/response/EnterpriseDetailsProfileResponse.java
+++ jobyte-api/src/main/java/br/com/gabriel/jobyte_api/enterprise/application/dtos/response/EnterpriseDetailsProfileResponse.java
@@ -1,12 +1,11 @@
 package br.com.gabriel.jobyte_api.enterprise.application.dtos.response;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 import br.com.gabriel.jobyte_api.enterprise.domain.entities.EnterpriseProfile;
 
 public record EnterpriseDetailsProfileResponse(
-  UUID id,
+  Long id,
   String companyName,
   String cnpj,
   String address,
