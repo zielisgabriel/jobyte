4204c42b9e1af261da1d319f412192399626575d
zielisgabriel <josegabrielas0586@email.com>
2025-12-26 19:29:34 -0300

    refactor(EntityNotFoundException): update constructor to use EnterpriseDetailsValueObject for better clarity

--- jobyte-api/src/main/java/br/com/gabriel/jobyte_api/shared/domain/exception/EntityNotFoundException.java
+++ jobyte-api/src/main/java/br/com/gabriel/jobyte_api/shared/domain/exception/EntityNotFoundException.java
@@ -1,13 +1,18 @@
 package br.com.gabriel.jobyte_api.shared.domain.exception;
 
+import br.com.gabriel.jobyte_api.enterprise.domain.valueobjects.EnterpriseDetailsValueObject;
 import jakarta.ws.rs.NotFoundException;
 
 public class EntityNotFoundException extends NotFoundException {
+  public EntityNotFoundException() {
+    super("Perfil não encontrada para o usuário autenticado.");
+  }
+
   public EntityNotFoundException(String message) {
     super(message);
   }
 
-  public EntityNotFoundException(String entity, Object id) {
-    super(String.format("%s não encontrado(a) com id: %s", entity, id));
+  public EntityNotFoundException(EnterpriseDetailsValueObject profile, Object id) {
+    super(String.format("%s não encontrado(a) com id: %s", profile, id));
   }
 }
