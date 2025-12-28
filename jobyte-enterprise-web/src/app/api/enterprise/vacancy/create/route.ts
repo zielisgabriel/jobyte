5551c727551021adc7bb426fc9f2bc071069eb34
zielisgabriel <josegabrielas0586@email.com>
2025-12-28 02:26:56 -0300

    feat(vacancy): implement POST route for creating vacancies with revalidation

--- jobyte-enterprise-web/src/app/api/enterprise/vacancy/create/route.ts
+++ jobyte-enterprise-web/src/app/api/enterprise/vacancy/create/route.ts
@@ -1,6 +1,14 @@
-import { apiProxy } from "@/middlewares/apiProxy";
+"use server";
+
+import { createVacancyService } from "@/services/create-vacancy-service";
+import { revalidateTag } from "next/cache";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
-  return apiProxy(req, "api/enterprise/vacancy/create")
+  const body = await req.json();
+  const { data, profileId } = body;
+  const response = await createVacancyService({ data, profileId });
+
+  revalidateTag("vacancies", "default");
+  return response;
 }
